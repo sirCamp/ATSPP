bcc3f944704100a646782e78b3dbd1045a95990e
Stefano Campese <stefano.campese@omnys.com>
2021-05-28 11:24:46 +0200

    fixes and container ultimating

--- job.sh
+++ job.sh
@@ -5,9 +5,9 @@
 #SBATCH --mail-user stefano.campese@phd.unipd.it
 #SBATCH --mail-type END,FAIL
 #SBATCH --partition allgroups
-#SBATCH --ntasks 16
-#SBATCH --mem 32G
+#SBATCH --ntasks 2
+#SBATCH --mem 2G
 #SBATCH --time 02:25:00
 #SBATCH –-gres=gpu:1
 cd $SLURM_SUBMIT_DIR
-srun singularity exec container.sif python script.py
+srun singularity exec container.sif python /opt/workspace/script.py
